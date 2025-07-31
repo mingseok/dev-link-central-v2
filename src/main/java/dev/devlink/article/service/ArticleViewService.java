87c611e75aac44dee1e8e3fc276648b49e90ac7c
kimminseok <alstjr706@naver.com>
2025-07-31 13:04:49 +0900

    refactor: 조회수 기능의 시간/공간 복잡도 개선

--- src/main/java/dev/devlink/article/service/ArticleViewService.java
+++ src/main/java/dev/devlink/article/service/ArticleViewService.java
@@ -37,6 +37,9 @@ public class ArticleViewService {
             redisTemplate.opsForValue()
                     .increment(RedisKey.getArticleViewKey(articleId));
 
+            redisTemplate.opsForSet()
+                    .add(RedisKey.flushTargetArticlesKey(), articleId.toString());
+
             redisTemplate.expire(
                     memberViewSetKey,
                     RedisConstants.DUPLICATE_PREVENTION_TTL,
@@ -58,21 +61,31 @@ public class ArticleViewService {
     @Transactional
     @Scheduled(fixedRate = RedisConstants.SYNC_INTERVAL_MILLIS)
     public void bulkUpdateViewCounts() {
-        Set<String> keys = redisTemplate.keys(RedisKey.getArticleViewKeys());
-        if (keys.isEmpty()) return;
+        Set<String> articleIds = redisTemplate.opsForSet()
+                .members(RedisKey.flushTargetArticlesKey());
+
+        if (articleIds == null) return;
 
         Map<Long, Long> redisViewCounts = new HashMap<>();
 
-        for (String key : keys) {
-            if (key.startsWith(RedisKey.getViewConcurrencyPrefix())) continue;
+        for (String articleIdStr : articleIds) {
+            Long articleId = Long.parseLong(articleIdStr);
+            String viewCountStr = redisTemplate.opsForValue()
+                    .get(RedisKey.getArticleViewKey(articleId));
 
-            String value = redisTemplate.opsForValue().get(key);
-            Long articleId = RedisKey.getArticleId(key);
-            Long viewCount = Long.parseLong(value);
+            if (viewCountStr == null) continue;
 
+            Long viewCount = Long.parseLong(viewCountStr);
             redisViewCounts.put(articleId, viewCount);
         }
 
         redisViewCounts.forEach(articleRepository::bulkAddViewCount);
+
+        for (Long articleId : redisViewCounts.keySet()) {
+            redisTemplate.delete(RedisKey.getArticleViewKey(articleId));
+
+            redisTemplate.opsForSet()
+                    .remove(RedisKey.flushTargetArticlesKey(), articleId.toString());
+        }
     }
 }
