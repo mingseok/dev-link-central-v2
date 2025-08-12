ddd43455102a5712c11c7c3a9b923fd152ece53d
kimminseok <alstjr706@naver.com>
2025-08-12 09:40:03 +0900

    refactor: Repository 수정/삭제 어노테이션 추가

--- src/main/java/dev/devlink/article/repository/ArticleLikeRepository.java
+++ src/main/java/dev/devlink/article/repository/ArticleLikeRepository.java
@@ -4,6 +4,7 @@ import dev.devlink.article.entity.Article;
 import dev.devlink.article.entity.ArticleLike;
 import dev.devlink.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> 
 
     long countByArticle(Article article);
 
+    @Modifying
     void deleteByArticleAndMember(Article article, Member member);
 
     Optional<ArticleLike> findByArticleAndMember(Article article, Member member);
--- src/main/java/dev/devlink/feed/repository/FeedLikeRepository.java
+++ src/main/java/dev/devlink/feed/repository/FeedLikeRepository.java
@@ -4,11 +4,13 @@ import dev.devlink.feed.entity.Feed;
 import dev.devlink.feed.entity.FeedLike;
 import dev.devlink.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
 
     long countByFeed(Feed feed);
 
+    @Modifying
     void deleteByFeedAndMember(Feed feed, Member member);
 
     boolean existsByFeedAndMember(Feed feed, Member member);
